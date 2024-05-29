d7845205e1c121bbd172f550de09abff7379eaff
Nyi Ye Zin <nyiyezin11@gmail.com>
2024-05-29 15:04:49 +0630

    feat: Add initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { DictionaryElement, Container } from "./styles/components";
 import GlobalStyles from "./styles/global";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <main>
-        <h1>Hello World!</h1>
-      </main>
+      <DictionaryElement>
+        <Container></Container>
+      </DictionaryElement>
       <GlobalStyles />
     </Provider>
   );
