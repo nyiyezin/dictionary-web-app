996a55d72c936636d9b0de000875376404fd3135
Nyi Ye Zin <nyiyezin11@gmail.com>
2024-05-30 13:41:22 +0630

    feat: Add redux middlware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,15 @@
 import { useDispatch } from "react-redux";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import themeReducer from "./themeSlice";
 import searchReducer from "./searchSlice";
@@ -18,6 +27,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
